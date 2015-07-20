d1f7835606249fa7597bb1c755e0b303e97bff0e
Nick DiQuattro <ndiquattro@gmail.com>
2015-07-19 23:27:44 -0700

    First commit

---
+++ app/views.py
@@ -0,0 +1,26 @@
+from flask import render_template
+from app import app
+from mcstatus import MinecraftServer
+
+@app.route('/mcb')
+def mcb():
+
+    # Look up server
+    server = MinecraftServer.lookup('98.244.54.58:25565')
+
+    # Ping the server
+    try:
+        ping = server.ping()
+    except:
+        stat = 'Down'
+        playnum = 'No'
+    else:
+        stat = 'Up'
+
+    if stat == 'Up':
+        # Get players info
+        playnum = server.status().players.online
+
+    return render_template('mcb.html',
+                           stat=stat,
+                           pnum=playnum)
