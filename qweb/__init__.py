4929e2dfe1270a5f91965d1553de37bd26c2a020
Nick DiQuattro <ndiquattro@gmail.com>
2015-09-15 01:24:23 -0700

    remove unneeded import

--- qweb/__init__.py
+++ qweb/__init__.py
@@ -8,5 +8,3 @@ qweb = Flask(__name__)
 # Register Blueprints
 qweb.register_blueprint(midnightstandings, url_prefix='/midnightstandings')
 qweb.register_blueprint(mcb, url_prefix='/mcb')
-
-import mcb, midnightstandings
